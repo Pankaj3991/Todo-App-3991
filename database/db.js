0469d5dc38729cdbc05efe473bf5429eb3ed4216
Pankaj Kumar <pankajbansal3991@gmail.com>
2023-09-04 22:46:18 +0530

    changed some content

--- database/db.js
+++ database/db.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 
 dotenv.config();
+
 const username = process.env.db_username;
 const password = process.env.db_password;
-const database = process.env.db_database;
 
 const Connection = ()=>{
-    const urlConnect = `mongodb+srv://${username}:${password}@cluster0.9ca9gxg.mongodb.net/${database}?retryWrites=true&w=majority`;
+    const urlConnect = `mongodb+srv://${username}:${password}@cluster0.9ca9gxg.mongodb.net/todo?retryWrites=true&w=majority`;
     mongoose.connect(urlConnect,{useNewUrlParser:true})
 
     mongoose.connection.on('connected', ()=>{
